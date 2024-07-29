e0862f1504ed757ea1b35f4981b8aa79fca6dc0e
Aviad Pineles <paviad2@gmail.com>
2024-07-29 15:39:10 +0300

    chore: removed secret management scripts

--- bin/bkup.cmd
+++
@@ -1,3 +0,0 @@
-git ls-files -o -i -X .gitsecrets | xargs tar -cvf apiauthorization-identityserver.tar
-if not exist "e:\secrets\" mkdir e:\secrets
-mv apiauthorization-identityserver.tar e:\secrets
--- bin/rstr.cmd
+++
@@ -1,3 +0,0 @@
-cp e:\secrets\apiauthorization-identityserver.tar .
-tar xvf apiauthorization-identityserver.tar
-del apiauthorization-identityserver.tar
